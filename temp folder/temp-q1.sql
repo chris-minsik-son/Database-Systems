473ed23a9adad681ef557b1eca3c723bf0bee58d
Min-Sik Son <chris.minsik.son@gmail.com>
2022-03-06 23:30:37 +1100

    q1 done

--- temp folder/temp-q1.sql
+++ temp folder/temp-q1.sql
@@ -2,4 +2,14 @@
 create or replace view Q1(unswid, name)
 as
 --... SQL statements, possibly using other views/functions defined by you ...
+
+    select
+		p.unswid,
+		p.name
+	from program_enrolments pe
+	join People p on (pe.student = p.id)
+	group by p.unswid, p.name
+	having count(distinct program) > 4
+	order by p.unswid asc
+
 ;
