6f84e9cd6783e0fb8aa3103467b851feedbaf5b0
Min-Sik Son <chris.minsik.son@gmail.com>
2022-03-14 01:57:39 +1100

    q7 done

--- ass1.sql
+++ ass1.sql
@@ -336,6 +336,17 @@ create or replace function
 	Q7(year integer, session text) returns table (code text)
 as $$
 --... SQL statements, possibly using other views/functions defined by you ...
+
+	select
+		s.code
+	--	c.term,
+	--	t.year,
+	--	t.session
+	from subjects s
+		join courses c on (s.id = c.subject)
+		join terms t on (c.term = t.id)
+	where s.code ~ 'COMP' and s.career = 'PG' and t.year = $1 and t.session = $2;
+
 $$ language sql;
 
 
