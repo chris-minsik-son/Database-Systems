7bb2f32c944906aaed451710429a359b10ec5d06
Min-Sik Son <chris.minsik.son@gmail.com>
2022-04-09 18:01:36 +1000

    fix

--- Movie Database Implementation/q1.py
+++ Movie Database Implementation/q1.py
@@ -14,11 +14,45 @@ db = None
 
 argc = len(sys.argv)
 
+N = sys.argv[1]
+N = int(N)
+
 # manipulate database
+
+query = """
+
+SELECT
+	Names.name,
+	COUNT(*) as count
+FROM crew_roles
+JOIN Names on (crew_roles.name_id = Names.id)
+WHERE crew_roles.role = 'director'
+GROUP BY Names.name
+ORDER BY count DESC;
+
+"""
 
 try:
 	db = psycopg2.connect("dbname=imdb")
+	cur = db.cursor()
+	cur.execute(query, N)
+	directors = cur.fetchall()
+
 	# ... add your code here ...
+
+	if type(N) != int or int(N) <0:
+		print(usage)
+		sys.exit(1)
+
+	count = 0
+
+	for record in directors:
+		if count == int(N):
+			break
+		print(record[1], record[0], count)
+		count = count + 1
+
+
 except psycopg2.Error as err:
 	print("DB error: ", err)
 finally:
